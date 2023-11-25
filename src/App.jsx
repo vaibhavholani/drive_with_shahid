1abe973bf1017c50dfda12ce7d283f1737880846
Vaibhav Holani <vaibhavholani@gmail.con>
2023-11-24 19:46:27 -0500

    milestone: first deployment version

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,23 @@ import Hero from './components/Hero';
 import CourseCard from './components/CourseCard';
 import Book from './components/Book';
 import GoogleReviews from './components/GoogleReviews';
+import FloatingCallButton from './components/FloatingCallButton';
+import Dropdown from './components/LocationDropdown';
+import CallSection from './components/CallSection';
 
 export default function App() {
+
+  const items = ["Item 1", "Item 2", "Item 3"];
+
   return (
     <>
     <Hero />
     <GoogleReviews />
     <CourseCard />
-    <Book />
+    {/* <Book /> */}
+    <FloatingCallButton />
+    {/* <Dropdown items={items} /> */}
+    <CallSection />
     </>
   )
 }
---
+++ src/components/CallSection.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import './CallSection.css'; // Make sure to create this CSS file
+
+const CallSection = () => {
+  const containerVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 }
+  };
+
+  const handleCallClick = () => {
+    window.location.href = 'tel:+16474009353'; // Use the phone number in the format 'tel:+[country code][number]'
+  };
+
+  return (
+    <motion.div
+      className="component-container mt-[20px]"
+      initial="hidden"
+      animate="visible"
+      variants={containerVariants}
+      transition={{ duration: 0.8 }}
+    >
+      <div className="horizontal-line"></div>
+      <div className="text-button-container">
+        <motion.p
+          className="responsive-text"
+          initial={{ scale: 0 }}
+          animate={{ rotate: 360, scale: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 260,
+            damping: 20
+          }}
+        >
+         <span className=""> Call </span>  Shahid and Book Now!:
+        </motion.p>
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="call-button"
+          onClick={handleCallClick}
+        >
+          Call
+        </motion.button>
+      </div>
+    </motion.div>
+  );
+};
+
+export default CallSection;
