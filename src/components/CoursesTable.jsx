ac46fae0d2eb769681cdd2bd0aedce430e35c43b
Vaibhav Holani <vaibhavholani@gmail.con>
2024-06-09 19:22:27 -0400

    fixed the button

--- src/components/CoursesTable.jsx
+++ src/components/CoursesTable.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./CoursesTable.css";
-import {packageCards} from "./PackageCardData"
+import { packageCards } from "./PackageCardData";
+import { getCalApi } from "@calcom/embed-react";
 
 export default function CoursesTable() {
   const [isPhoneView, setIsPhoneView] = useState(window.innerWidth <= 768);
@@ -12,38 +13,59 @@ export default function CoursesTable() {
   useEffect(() => {
     window.addEventListener("resize", handleResize);
     // Cleanup function to remove the event listener
+    (async function () {
+      const cal = await getCalApi();
+      cal("ui", {
+        theme: "light",
+        styles: { branding: { brandColor: "#2e6434" } },
+        hideEventTypeDetails: false,
+        layout: "month_view",
+      });
+    })();
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
-    <div className={`overflow-x-auto max-w-screen-lg mx-auto ${isPhoneView ? 'px-5' : 'px-0'}`}>
+    <div
+      className={`overflow-x-auto max-w-screen-lg mx-auto ${
+        isPhoneView ? "px-5" : "px-0"
+      }`}
+    >
       <div className="max-w-full mx-auto">
         <table className="min-w-full bg-[#c4ddc7] md:bg-white">
           <thead className="bg-[#2E6434] text-white">
             <tr>
               <th
-                className={`py-2 mr-2 font-bold ${isPhoneView ? 'text-sm' : 'text-base'}`}
+                className={`py-2 mr-2 font-bold ${
+                  isPhoneView ? "text-sm" : "text-base"
+                }`}
                 style={{ fontFamily: "Inter, sans-serif" }}
               >
                 {isPhoneView ? "Course" : "Course"}
               </th>
               <th className="md:m-0.5 hidden md:table-cell bg-white"></th>
               <th
-                className={`py-2 font-bold ${isPhoneView ? 'text-sm' : 'text-base'}`}
+                className={`py-2 font-bold ${
+                  isPhoneView ? "text-sm" : "text-base"
+                }`}
                 style={{ fontFamily: "Inter, sans-serif" }}
               >
                 {isPhoneView ? "Hours" : "In-Class Hours"}
               </th>
               <th className="md:m-0.5 hidden md:table-cell bg-white"></th>
               <th
-                className={`py-2 pl-2 md:pl-0 font-bold ${isPhoneView ? 'text-sm' : 'text-base'}`}
+                className={`py-2 pl-2 md:pl-0 font-bold ${
+                  isPhoneView ? "text-sm" : "text-base"
+                }`}
                 style={{ fontFamily: "Inter, sans-serif" }}
               >
                 {isPhoneView ? "Car" : "Road Test Car"}
               </th>
               <th className="md:m-0.5 hidden md:table-cell bg-white"></th>
               <th
-                className={`py-2 px-6 md:px-0 font-bold ${isPhoneView ? 'text-sm' : 'text-base'}`}
+                className={`py-2 px-6 md:px-0 font-bold ${
+                  isPhoneView ? "text-sm" : "text-base"
+                }`}
                 style={{ fontFamily: "Inter, sans-serif" }}
               >
                 Price
@@ -60,11 +82,27 @@ export default function CoursesTable() {
           >
             {packageCards.map((pkg, index) => (
               <tr key={index} className="border-none">
-                <td className={`py-2 text-center ${isPhoneView ? 'text-sm' : 'text-base'}`}>{pkg.title}</td>
+                <td
+                  className={`py-2 text-center ${
+                    isPhoneView ? "text-sm" : "text-base"
+                  }`}
+                >
+                  {pkg.title}
+                </td>
                 <td className="md:m-0.5 hidden md:table-cell bg-white"></td>
-                <td className={`py-2 text-center ${isPhoneView ? 'text-sm' : 'text-base'}`}>{pkg.features[0].split(' ')[0]}</td>
+                <td
+                  className={`py-2 text-center ${
+                    isPhoneView ? "text-sm" : "text-base"
+                  }`}
+                >
+                  {pkg.features[0].split(" ")[0]}
+                </td>
                 <td className="md:m-0.5 hidden md:table-cell bg-white"></td>
-                <td className={`py-2 text-center ${isPhoneView ? 'text-sm' : 'text-base'}`}>
+                <td
+                  className={`py-2 text-center ${
+                    isPhoneView ? "text-sm" : "text-base"
+                  }`}
+                >
                   <svg
                     className="w-6 h-6 mx-auto pl-1 md:pl-0"
                     fill="black"
@@ -79,12 +117,22 @@ export default function CoursesTable() {
                   </svg>
                 </td>
                 <td className="md:m-0.5 hidden md:table-cell bg-white"></td>
-                <td className={`py-2 text-center ${isPhoneView ? 'text-sm' : 'text-base'}`}>${pkg.totalPrice}</td>
-                <td className={`py-2 text-center ${isPhoneView ? 'text-sm' : 'text-base'}`}>
-                  <button 
-                  data-cal-link="drivewithshahid/1-hour-in-person-driving-lesson"
-                  data-cal-config='{"layout":"month_view"}'
-                  className="border border-[#2E6434] text-[#2E6434] px-[10px] py-[4px] rounded-[10px] hover:bg-[#2E6434] hover:text-white transition duration-200"
+                <td
+                  className={`py-2 text-center ${
+                    isPhoneView ? "text-sm" : "text-base"
+                  }`}
+                >
+                  ${pkg.totalPrice}
+                </td>
+                <td
+                  className={`py-2 text-center ${
+                    isPhoneView ? "text-sm" : "text-base"
+                  }`}
+                >
+                  <button
+                    data-cal-link="drivewithshahid/1-hour-in-person-driving-lesson"
+                    data-cal-config='{"layout":"month_view"}'
+                    className="border border-[#2E6434] text-[#2E6434] px-[10px] py-[4px] rounded-[10px] hover:bg-[#2E6434] hover:text-white transition duration-200"
                   >
                     Select
                   </button>
